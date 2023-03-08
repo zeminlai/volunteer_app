6a4158040fa9b0551814ea6b6c7e7261af290bbe
Lai Ze Min <zeminlai426@gmail.com>
2023-03-08 20:30:49 +0800

    make structure for find tutor page

--- lib/pages/find_tutor_page.dart
+++ lib/pages/find_tutor_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:volunteer_app/utils/find_tutor_components.dart';
+import 'package:volunteer_app/utils/size_config.dart';
 
 class FindTutorPage extends StatefulWidget {
   const FindTutorPage({super.key});
@@ -11,8 +12,48 @@ class FindTutorPage extends StatefulWidget {
 class _FindTutorPageState extends State<FindTutorPage> {
   @override
   Widget build(BuildContext context) {
-    List<String> subjects = ["SPM", "IGCSE", "PT3"];
-    String dropdownValue = subjects[0];
+    ScreenSize().init(context);
+    List<String> level = ["SPM", "IGCSE", "PT3"];
+    List<String> subjects = [
+      "Maths",
+      "Physics",
+      "BM",
+      "Add Maths",
+      "Biology",
+      "Sejarah"
+    ];
+    String dropdownValue = level.first;
+
+    List<Map<String, dynamic>> tutorInfo = [
+      {
+        "name": "TanJZ",
+        "uni": "Universiti Malaya",
+        "stars": 5.0,
+        "image":
+            "https://scontent.fbki2-1.fna.fbcdn.net/v/t1.6435-9/31817390_1448910555255518_2611503638257860608_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=174925&_nc_ohc=-AcsCtl_ISIAX96Sr3k&_nc_oc=AQltIfNhMd8kd0PkRS7R6UbC9ORVT48fRNjCQPmkEQcqDJVtDoNwItcyTet-CVRLSh-Vdpr9HUPWxqaC238jyg2l&_nc_ht=scontent.fbki2-1.fna&oh=00_AfDgrR0z1TAJpm0W2T-FgVOBqNuuvzuq3jdZdhzg6g-PnA&oe=642FBF28",
+      },
+      {
+        "name": "TanJZ",
+        "uni": "Universiti Malaya",
+        "stars": 5.0,
+        "image":
+            "https://scontent.fbki2-1.fna.fbcdn.net/v/t1.6435-9/31817390_1448910555255518_2611503638257860608_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=174925&_nc_ohc=-AcsCtl_ISIAX96Sr3k&_nc_oc=AQltIfNhMd8kd0PkRS7R6UbC9ORVT48fRNjCQPmkEQcqDJVtDoNwItcyTet-CVRLSh-Vdpr9HUPWxqaC238jyg2l&_nc_ht=scontent.fbki2-1.fna&oh=00_AfDgrR0z1TAJpm0W2T-FgVOBqNuuvzuq3jdZdhzg6g-PnA&oe=642FBF28",
+      },
+      {
+        "name": "TanJZ",
+        "uni": "Universiti Malaya",
+        "stars": 5.0,
+        "image":
+            "https://scontent.fbki2-1.fna.fbcdn.net/v/t1.6435-9/31817390_1448910555255518_2611503638257860608_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=174925&_nc_ohc=-AcsCtl_ISIAX96Sr3k&_nc_oc=AQltIfNhMd8kd0PkRS7R6UbC9ORVT48fRNjCQPmkEQcqDJVtDoNwItcyTet-CVRLSh-Vdpr9HUPWxqaC238jyg2l&_nc_ht=scontent.fbki2-1.fna&oh=00_AfDgrR0z1TAJpm0W2T-FgVOBqNuuvzuq3jdZdhzg6g-PnA&oe=642FBF28",
+      },
+      {
+        "name": "TanJZ",
+        "uni": "Universiti Malaya",
+        "stars": 5.0,
+        "image":
+            "https://scontent.fbki2-1.fna.fbcdn.net/v/t1.6435-9/31817390_1448910555255518_2611503638257860608_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=174925&_nc_ohc=-AcsCtl_ISIAX96Sr3k&_nc_oc=AQltIfNhMd8kd0PkRS7R6UbC9ORVT48fRNjCQPmkEQcqDJVtDoNwItcyTet-CVRLSh-Vdpr9HUPWxqaC238jyg2l&_nc_ht=scontent.fbki2-1.fna&oh=00_AfDgrR0z1TAJpm0W2T-FgVOBqNuuvzuq3jdZdhzg6g-PnA&oe=642FBF28",
+      },
+    ];
     return Scaffold(
       body: Container(
         width: double.infinity,
@@ -25,13 +66,54 @@ class _FindTutorPageState extends State<FindTutorPage> {
         ),
         child: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              //FIND TUTOR TITLE
-              Text("Find a tutor"),
-              // LEVEL SELECTION INPUT FIELD
-              SubjectDropdown(subjects: subjects, dropdownValue: dropdownValue)
+              Padding(
+                padding: EdgeInsets.fromLTRB(ScreenSize.horizontal! * 5,
+                    ScreenSize.vertical! * 5, ScreenSize.horizontal! * 5, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //FIND TUTOR TITLE
+                    Text(
+                      "Find a tutor",
+                      style: TextStyle(
+                          fontSize: ScreenSize.vertical! * 4,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    SizedBox(
+                      height: ScreenSize.vertical! * 2,
+                    ),
+
+                    // LEVEL SELECTION INPUT FIELD
+                    SubjectDropdown(level: level, dropdownValue: dropdownValue),
+                  ],
+                ),
+              ),
+
               // LIST OF TUTORS
+              Container(
+                padding: EdgeInsets.all(ScreenSize.horizontal! * 5),
+                width: ScreenSize.horizontal! * 100,
+                height: ScreenSize.vertical! * 78,
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(155, 255, 255, 255),
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(40),
+                        topLeft: Radius.circular(40))),
+                child: Column(
+                  children: [
+                    //Subjects
+                    SubjectSelection(
+                      subjects: subjects,
+                    ),
+
+                    //Tutors
+                    FindTutorSection(tutorInfo: tutorInfo)
+                  ],
+                ),
+              )
             ],
           ),
         ),
--- lib/utils/find_tutor_components.dart
+++ lib/utils/find_tutor_components.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:volunteer_app/utils/size_config.dart';
 
+// Subject dropdown menu
 class SubjectDropdown extends StatefulWidget {
-  List<String> subjects;
+  List<String> level;
   String dropdownValue;
 
   SubjectDropdown(
-      {super.key, required this.subjects, required this.dropdownValue});
+      {super.key, required this.level, required this.dropdownValue});
 
   @override
   State<SubjectDropdown> createState() => Subject_DropdownState();
@@ -14,27 +16,117 @@ class SubjectDropdown extends StatefulWidget {
 class Subject_DropdownState extends State<SubjectDropdown> {
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<String>(
+    return DropdownButtonFormField(
+      decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+            borderSide:
+                BorderSide(color: Color.fromARGB(155, 255, 255, 255), width: 2),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+            borderSide:
+                BorderSide(color: Color.fromARGB(1, 255, 255, 255), width: 2),
+          ),
+          filled: true,
+          fillColor: Color.fromARGB(155, 255, 255, 255),
+          contentPadding: EdgeInsets.all(10)),
+      isExpanded: true,
       value: widget.dropdownValue,
-      icon: const Icon(Icons.arrow_downward),
-      elevation: 16,
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
-      ),
-      onChanged: (String? value) {
-        // This is called when the user selects an item.
-        setState(() {
-          widget.dropdownValue = value!;
-        });
-      },
-      items: (widget.subjects).map<DropdownMenuItem<String>>((String value) {
+      items: widget.level.map<DropdownMenuItem<String>>((String value) {
         return DropdownMenuItem<String>(
           value: value,
-          child: Text(value),
+          child: Center(
+            child: Text(
+              value,
+              style: TextStyle(fontSize: 15),
+            ),
+          ),
         );
       }).toList(),
+      onChanged: (value) {
+        setState(
+          () {
+            widget.dropdownValue = value!;
+          },
+        );
+      },
+    );
+  }
+}
+
+// Subject selection
+class SubjectSelection extends StatefulWidget {
+  List<String> subjects;
+
+  SubjectSelection({super.key, required this.subjects});
+
+  @override
+  State<SubjectSelection> createState() => _SubjectSelectionState();
+}
+
+class _SubjectSelectionState extends State<SubjectSelection> {
+  late String subjectSelected = widget.subjects.first;
+
+  @override
+  Widget build(BuildContext context) {
+    ScreenSize().init(context);
+    return Container(
+      width: ScreenSize.horizontal! * 100,
+      height: ScreenSize.vertical! * 4,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: widget.subjects.map((value) {
+          return TextButton(
+            onPressed: () {
+              setState(() {
+                subjectSelected = value;
+                print(subjectSelected);
+              });
+            },
+            child: (subjectSelected == value)
+                ? Text(
+                    value,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: ScreenSize.vertical! * 2),
+                  )
+                : Text(
+                    value,
+                    style: TextStyle(
+                        color: Colors.grey, fontSize: ScreenSize.vertical! * 2),
+                  ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
+
+class FindTutorSection extends StatefulWidget {
+  List<Map<String, dynamic>> tutorInfo = [{}];
+  FindTutorSection({super.key, required this.tutorInfo});
+
+  @override
+  State<FindTutorSection> createState() => _FindTutorSectionState();
+}
+
+class _FindTutorSectionState extends State<FindTutorSection> {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: widget.tutorInfo.length,
+        itemBuilder: (context, index) {
+          final tutor = widget.tutorInfo[index];
+          return ListTile(
+            leading: Image.network(tutor["image"]),
+            title: Text(tutor["name"]),
+            subtitle: Text(tutor["uni"]),
+            trailing: Text("${tutor["stars"]}"),
+          );
+        },
+      ),
     );
   }
 }
